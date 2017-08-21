cd8dd6e922f8553b918963f1b3e529bfcf4b69b4
Paul <comixan@gmail.com>
2017-08-21 21:41:58 +0300

    Update setup.py
    little update.

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
-#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
 setup(
     name='liqpay-python3',
-    version='1.1',
-    description='LiqPay Python3 SDK',
+    version='1.1.1',
+    author='PaulGregor',
+    description='LiqPay SDK for Python 3',
     packages=find_packages(),
     include_package_data=True,
+    classifiers=[
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
     install_requires=['requests']
 )
